022f82d8f6e7261b0b021b92dbb5ce9888aae488
Francesco Amato <francescoamato@MBPdiFrancesco.fritz.box>
2024-11-30 07:20:06 +0100

    Replace call_llm externally

--- src/app.py
+++ src/app.py
@@ -2,38 +2,11 @@ import streamlit as st
 import pandas as pd
 import os
 from jinja2 import Environment, FileSystemLoader
-from utils import init_LLM, testo_to_utf8
+from utils import init_LLM, call_llm, testo_to_utf8
 
 # Initialize the LLM with the Google API key from secrets
 llm = init_LLM(API_KEY=st.secrets["GROQ_API_KEY"])
 llm_model_name = "llama3-70b-8192"
-
-
-def call_llm(sys_message: str, context_message: str, base64_image: str = "", 
-            temperature: float = 0.5, max_tokens: int = None, top_p: float = 0.8, stop: str = None) -> str:
-    if base64_image == "":
-        messages = [
-            {"role": "system", "content": sys_message},
-            {"role": "user", "content": context_message}
-        ]
-    else:
-        messages = [
-            {"role": "system", "content": sys_message},
-            {"role": "user", "content": context_message},
-            {"role": "image_url", "image_url": {"url": f"data:image/png;base64,{base64_image}"}}
-        ]
-
-    response_stream = llm.chat.completions.create(
-        model=llm_model_name,
-        messages=messages,
-        temperature=temperature,
-        max_tokens=max_tokens,
-        top_p=top_p,
-        stop=None,
-        stream=True
-    )
-
-    return response_stream
 
 
 # Load the Jinja template from the file
@@ -74,7 +47,7 @@ def main():
             st.markdown(query)
         # Call the LLM with the Jinja prompt and DataFrame context
         with st.chat_message("assistant"):        
-            stream = call_llm(sys_message, f'"{query}"')
+            stream = call_llm(llm, llm_model_name, sys_message, f'"{query}"')
 
             # Initialize an empty string to store the full response as it is built
             response = ""
--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,32 @@ def init_LLM(API_KEY=None):
     return client
 
 
+def call_llm(llm, llm_model_name, sys_message: str, context_message: str, base64_image: str = "", 
+            temperature: float = 0.5, max_tokens: int = None, top_p: float = 0.8, stop: str = None) -> str:
+    if base64_image == "":
+        messages = [
+            {"role": "system", "content": sys_message},
+            {"role": "user", "content": context_message}
+        ]
+    else:
+        messages = [
+            {"role": "system", "content": sys_message},
+            {"role": "user", "content": context_message},
+            {"role": "image_url", "image_url": {"url": f"data:image/png;base64,{base64_image}"}}
+        ]
+
+    response_stream = llm.chat.completions.create(
+        model=llm_model_name,
+        messages=messages,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        top_p=top_p,
+        stop=None,
+        stream=True
+    )
+
+    return response_stream
+
 mapping = {
     'Ã¨': 'è',
     'Ã ': 'à',
