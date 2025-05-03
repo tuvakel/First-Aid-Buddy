0e108f491cbca9dc13dfffea85e737ff894fe427
tuvakel <tuvacharo@gmail.com>
2025-05-03 10:12:48 +0300

    Update triage_utils.py

--- src/triage_utils.py
+++ src/triage_utils.py
@@ -8,7 +8,8 @@ import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain_community.retrievers import BM25Retriever, EnsembleRetriever
+from langchain_community.retrievers import BM25Retriever
+from langchain.retrievers import EnsembleRetriever
 from concurrent.futures import ThreadPoolExecutor
 from langchain.schema import Document
 import re
